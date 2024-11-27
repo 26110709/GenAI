4fa434c66c4f7fccccd8ae1e726889df99488647
26110709 <prashant.aryan21@vit.edu>
2024-11-27 16:28:00 +0530

    final commit

--- app.py
+++ app.py
@@ -6,5 +6,23 @@ from langchain_core.prompts import ChatPromptTemplate
 load_dotenv()
 from langchain_core.output_parsers import StrOutputParser
 
+os.environ["langchain_api"] = os.getenv("langchain_api")
+os.environ["langchain_project"] = os.getenv("langchain_project")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","you are a helpful assistant. Please respond to the question asked"),
+        ("user","Question:{question}")
+    ]
+)
 
+st.title("Langchain demo with Llama3.1")
+input_text=st.text_input("How can I help you?")
+
+llm = Ollama(model = "llama3.1")
+output_parser = StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
